bb8dc044abae0e412c4a3df9be29fc7b4e2efb22
Julián Santa Ana <juliansantaana@gmail.com>
2013-11-01 19:36:52 -0300

    This is the end, aqui se decide todo, vamos a morir. chau.

--- src/dptoalumnos/Main.java
+++ src/dptoalumnos/Main.java
@@ -3,7 +3,7 @@ package dptoalumnos;
 import javax.swing.JOptionPane;
 
 public class Main {
-
+    //this is it.
     public static void main(String[] args) {
         Modelo m = new Modelo();
         Vista v = new Vista();
